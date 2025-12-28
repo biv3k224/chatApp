9c5caa8c9a27e87300e48ba35e1e899a0031e441
Bibek Kumar Tamang <bibek@MacBookPro.lan>
2025-12-28 14:42:30 -0500

    frontend added and connected

--- backend/src/main/java/com/chat/controller/RoomController.java
+++ backend/src/main/java/com/chat/controller/RoomController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://localhost:5173")
 @RequestMapping("/api/v1/rooms")
 public class RoomController {
 
